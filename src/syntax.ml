6c554fd9a3df66659d6e5349f0a04db60ec70f75
Andrew Duke <shweduke@mail.ru>
2011-08-22 23:33:27 +0800

    fixed formatting

--- src/syntax.ml
+++ src/syntax.ml
@@ -1,23 +1,23 @@
 type expression =
-	| Numeral of int
-	| Plus of expression * expression
-	| Minus of expression * expression
-	| Times of expression * expression
-	| Divide of expression * expression
-	| Negate of expression
-	| String of string
-	| Concatenation of expression * expression
-	| Boolean of bool
-	| Or of expression * expression
-	| And of expression * expression
+  | Numeral of int
+  | Plus of expression * expression
+  | Minus of expression * expression
+  | Times of expression * expression
+  | Divide of expression * expression
+  | Negate of expression
+  | String of string
+  | Concatenation of expression * expression
+  | Boolean of bool
+  | Or of expression * expression
+  | And of expression * expression
 
 type value =
-	| Bool of bool
-	| Int of int
-	| Str of string
+  | Bool of bool
+  | Int of int
+  | Str of string
 
 let value_to_string v =
-	match v with
-	| Bool b -> string_of_bool b
-	| Int i -> string_of_int i
-	| Str s -> s
+  match v with
+  | Bool b -> string_of_bool b
+  | Int i -> string_of_int i
+  | Str s -> s
